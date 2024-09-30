63f57fa4c3bbbb676244ad31f83a7313f853a457
origami01 <dixonhdo@gmail.com>
2024-09-30 19:47:28 +0300

    create middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -29,3 +29,13 @@ app.use('/api/auth', authRouter);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const mesaage = err.message || "Internet Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
